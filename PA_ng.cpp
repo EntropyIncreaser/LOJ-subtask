960d9b5d28b494a9858f626eabd13252627b8a6a
EntropyIncreaser <2743134282@qq.com>
2021-01-04 17:22:03 +0800

    Create PA_ng.cpp

---
+++ PA_ng.cpp
@@ -0,0 +1,50 @@
+// PA -> LOJ
+
+#include <cstdlib>
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	system("ls > tmp.txt");
+
+	ifstream tmp("tmp.txt");
+
+	static char mx[11];
+	string title, filename;
+	while (tmp >> filename) {
+		if (filename.size() < 5) continue;
+		if (filename.substr(filename.size() - 3) == ".in") {
+			int p = 0;
+			while (!isdigit(filename[p])) ++p;
+			title = filename.substr(0, p);
+			if (filename[p] != '0') {
+				int tc = atoi(filename.substr(p, filename.size() - p - 4).c_str());
+				mx[tc] = max(mx[tc], filename[filename.size() - 4]);
+				//cerr << filename << ' ' << title << ' ' << tc << ' ' << filename[filename.size() - 4] << '\n';
+			}
+		}
+	}
+
+	ofstream data("data.yml");
+/*
+	data <<
+"subtasks:\n"
+"  - score: 0\n"
+"    type: min\n"
+"    cases: ['0']\n";
+*/
+	for (int i = 1; i <= 10; ++i) {
+		data <<
+"  - scoringType: GroupMin\n"
+"    testcases:\n";
+		for (char c = 'a'; c <= mx[i]; ++c) {
+			data << "      - inputFile: " << title << i << c << ".in\n"
+			"        outputFile: " << title << i << c << ".out\n";
+		}
+	}
+
+	return 0;
+}
